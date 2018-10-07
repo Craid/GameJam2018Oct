c87272b797e8dfb71e24a60f33e9ec53e25d0de0
Michel <michel.schmitz_1992@hotmail.com>
2018-10-07 17:03:40 +0200

    Done

---
+++ UnityGame/Assets/Michel/Scripts/MenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	public GameObject title;
+	public GameObject buttons;
+	public GameObject credits;
+
+	public void onPlayClick(){
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void onCreditsClick(){
+		credits.SetActive (true);
+		buttons.SetActive (false);
+		title.SetActive (false);
+	}
+
+	public void onBackToMenu(){
+		credits.SetActive (false);
+		buttons.SetActive (true);
+		title.SetActive (true);
+	}
+
+	public void onExitClick(){
+		Application.Quit ();
+	}
+
+}
